f5c00b24ede58919f628f8ed5621c54896fdd7c5
memorisanka <j.krawczyk87@gmail.com>
2022-07-29 14:27:31 +0200

    feat: complete change of menu

--- functionality/menu.py
+++ functionality/menu.py
@@ -1,13 +1,20 @@
+from __future__ import annotations
+
+
 class Menu:
     def __init__(self):
-        self.executor = Executor()
+        self._encrypt = encrypt or Encrypt()
+        self._decrypt = decrypt or Decrypt()
         self.options = {
-            1: self.executor.enter_text_to_encrypt,
-            2: self.executor.encrypt_rot13,
-            3: self.executor.encrypt_rot47,
-            4: self.executor.decrypt,
-            5: self.executor.exit_programm,
+            1: self._encrypt.enter_text_to_encrypt,
+            2: self._encrypt.encrypt_rot13,
+            3: self._encrypt.encrypt_rot47,
+            4: "self.executor.decrypt",
+            5: menu.exit_programm(),
         }
+
+    def operation(self) -> None:
+        menu.show_menu()
 
     def show_menu(self) -> None:
         choice = int(
@@ -25,51 +32,56 @@ class Menu:
     def execute(self, choice: int) -> None:
         self.options.get(choice, self.show_error)()
 
-
-class Executor:
-    @staticmethod
-    def enter_text_to_encrypt():
-        text_to_encrypt = input("Enter text: ")
-        Executor.write_text(text_to_encrypt)
-        return text_to_encrypt
-
-    def encrypt_rot13(self, text):
-        ROT13 = 13
-        encrypted_text_rot13 = ""
-        for letter in range(len(text)):
-            if ord(text[letter]) > 122 - ROT13:
-                encrypted_text_rot13 += chr(ord(text[letter]) + ROT13 - 26)
-            else:
-                encrypted_text_rot13 += chr(ord(text[letter]) + ROT13)
-        return encrypted_text_rot13
-
-    def encrypt_rot47(self, text):
-        encrypted_text_rot47 = ""
-        for letter in range(len(text)):
-            j = ord(text[letter])
-            if 33 <= j <= 126:
-                encrypted_text_rot47 += chr(33 + (j + 14) % 94)
-            else:
-                encrypted_text_rot47 += text[letter]
-        return encrypted_text_rot47
-
-    def decrypt(self):
-        pass
-
     @staticmethod
     def exit_programm():
         print("Bye. Hope to see you again :)")
         exit()
 
-    def write_text(self, text):
-        with open ("text_to_encrypt.txt", "-w", encoding="utf-8") as f:
-            f.write(text)
+
+class Encrypt:
+    def __init__(self):
+        self.text = ""
+
+    def enter_text_to_encrypt(self):
+        self.text = input("Enter text: ")
+
+    def encrypt_rot13(self):
+        ROT13 = 13
+        encrypted_text_rot13 = ""
+        for letter in range(len(self.text)):
+            if ord(self.text[letter]) > 122 - ROT13:
+                encrypted_text_rot13 += chr(ord(self.text[letter]) + ROT13 - 26)
+            else:
+                encrypted_text_rot13 += chr(ord(self.text[letter]) + ROT13)
+        print(encrypted_text_rot13)
+        return encrypted_text_rot13
+
+    def encrypt_rot47(self):
+        encrypted_text_rot47 = ""
+        for letter in range(len(self.text)):
+            j = ord(self.text[letter])
+            if 33 <= j <= 126:
+                encrypted_text_rot47 += chr(33 + (j + 14) % 94)
+            else:
+                encrypted_text_rot47 += self.text[letter]
+        print(encrypted_text_rot47)
+        return encrypted_text_rot47
 
 
-def main():
-    menu = Menu()
-    menu.show_menu()
+class Decrypt:
+    def operation1(self) -> str:
+        pass
+
+    def operation_z(self) -> str:
+        pass
+
+
+def client_code(menu: Menu) -> None:
+    print(menu.operation(), end="")
 
 
 if __name__ == "__main__":
-    main()
+    encrypt = Encrypt()
+    decrypt = Decrypt()
+    menu = Menu()
+    client_code(menu)
