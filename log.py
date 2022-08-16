c2601475bfa564671571cea15475f82d9ad096fd
memorisanka <j.krawczyk87@gmail.com>
2022-08-16 20:01:18 +0200

    feat(log): add 3 trials, when user enter wrong username or password

--- log.py
+++ log.py
@@ -6,6 +6,7 @@ from input_output_handler import InputOutputHandler as io
 class UserLog:
     def __init__(self):
         self.users_buffer = {}
+        self.counter = 0
 
     def add(self, usr: str, pw: str) -> None:
         self.users_buffer.update({usr: pw})
@@ -19,15 +20,20 @@ class UserLog:
         return False
 
     def log(self) -> bool:
-        user_name: str = io.read("User name: ")
-        password: str = io.read("Password: ")
-        hash_password: str = Rot47.cipher(password)
-        if DataBase.check_user(user_name, hash_password):
-            io.print_text("Logged in!", "\n")
-            return True
+        while self.counter < 3:
+            user_name: str = io.read("User name: ")
+            password: str = io.read("Password: ")
+            hash_password: str = Rot47.cipher(password)
+            if DataBase.check_user(user_name, hash_password):
+                io.print_text("Logged in!", "\n")
+                return True
+            else:
+                self.counter += 1
+                io.print_text("Invalid username or password", "Try again", f"Trials left: {3 - self.counter}")
+                self.log()
         else:
-            io.print_text("Invalid username or password", "Try again")
-            self.log()
+            io.print_text("All trials were used. You have to run program again.")
+            exit()
 
     def new_user(self) -> None:
         user_name: str = io.read("User name: ")
