ae9f04c91a7d1b0deebcf0ccf600874a52213c19
memorisanka <j.krawczyk87@gmail.com>
2022-08-27 10:05:34 +0200

    tests(buffer): check, if function create_dict creates dictionary from buffer

--- tests/test_buffer.py
+++ tests/test_buffer.py
@@ -7,7 +7,7 @@ class TestBuffer:
         self.test_buffer = Buffer()
 
     def test_add_to_buffer(self):
-        """Test should add record to buffer."""
+        """Should add record to buffer."""
 
         self.test_buffer.add({"Test": "test"})
         assert self.test_buffer.__len__() == 1
@@ -15,7 +15,16 @@ class TestBuffer:
         self.test_buffer.add({"Test1": "test1"})
         assert self.test_buffer.__len__() == 2
 
-    def test_peak_buffer(self):
-        """Should print buffer_dict's content."""
+        self.test_buffer.add({"Test1": "test_test"})
+        assert self.test_buffer.__len__() == 3
 
-        assert self.test_buffer.peak() == {"Test": ["test"], "Test1": ["test1"]}
+        self.test_buffer.add({"Test": "test_test_test"})
+        assert self.test_buffer.__len__() == 4
+
+    def test_peak_buffer(self):
+        """Should return buffer_dict's content."""
+
+        assert self.test_buffer.peak() == {
+            "Test": ["test", "test_test_test"],
+            "Test1": ["test1", "test_test"],
+        }
