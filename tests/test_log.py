01559a31854364d1597b884e8b86feadc40e3724
memorisanka <j.krawczyk87@gmail.com>
2022-09-03 21:08:41 +0200

    tests(log): small changes in code

--- tests/test_log.py
+++ tests/test_log.py
@@ -6,15 +6,18 @@ from unittest.mock import Mock
 class TestLog:
     @patch("builtins.input", side_effect=["1", "2"])
     def test_log_manager(self, input):
+        """If the input is 1, function should return UserLog.log(),
+        if the input is 2, function should return UserLog.new_user()"""
 
         UserLog.log = Mock(return_value="log_test.log")
-        m = UserLog()
+        ul = UserLog()
 
-        assert m.log_manager() == m.log()
-        del m
+        assert ul.log_manager() == ul.log()
+        del ul
 
         UserLog.new_user = Mock(return_value="log_test.new_user")
-        m = UserLog()
+        ul = UserLog()
 
-        assert m.log_manager() == m.new_user()
-        del m
+        assert ul.log_manager() == ul.new_user()
+        del ul
+
